0c9b24375f52ea570391c95267ba3fc365cb39d5
vinayv_456 <vinayvallabineni456@gmail.com>
2021-03-16 18:49:24 +0530

    Add nested div for showing the progress

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import './App.css';
 function App() {
   const [activeProductIndex, setActiveProductIndex] = useState(0);
   const [products , setProducts] = useState([])
-  
+  const [percentage, setPercentage] = useState(0)
+
   const [formTarget, setFromTarget] = useState('');
   const [formAchieved, setFormAchieved] = useState('');
   
@@ -60,29 +61,26 @@ function App() {
   }, [])
   
   const getPercentage = (index) => {
-    return (products[index]?.achieved/products[index]?.target)*100
-  }
+    setPercentage((products[index]?.achieved/products[index]?.target)*100)
 
+  }
+  
   return (
-    <div className="main" style={{height: '100vh', overflow:'hidden'}}>
-      <div className="App" style={{overflowX: 'scroll', position:'relative'}}>
+    <div className="main" style={{height: '100vh', display:'flex', width:'100vw'}}>
+      <div className="App" style={{position:'relative', flexDirection:'column', overflowY:'scroll', width:'80%'}}>
         {
           products?.map((product, key) => {
             return <div key={key} className={`card testGradiants${key%3} flexVertical`}  onClick={()=> {setActiveProductIndex(key); setFromTarget(product?.target); setFormAchieved(product?.achieved);}}>
-                 {/* <div key={key} className={`card testGradiants${key%3} flexVertical`} style={{position:'absolute', height:'100px'}} >  */}
-                  
-                   
-                  <h2 className={'center'}>{product.name}</h2>
-                  <p>{product.target ? product.target : '--'}/{product.achieved ? product.achieved : '--'}</p>
+                    <h2 className={'center'}>{product.name}</h2>
+                    <div style={{ height: percentage}} className={`transition`}></div>
+                    <p>{product.target ? product.target : '--'}/{product.achieved ? product.achieved : '--'}</p>
                   </div>
-                // </div>
           })
         }
       </div>
 
       {products&&
-      <div style={{display:'flex', height:'100%'}}>
-          <div style={{width:'50%', height:'100%', borderRight: '3px solid black'}}>
+      <div style={{display:'flex', justifyContent:'center', alignItems:'center', flexDirection:"column"}}>
             <h2 style={{textAlign:'center', marginTop:'30px', marginBottom:'50px'}}>Enter details of {products[activeProductIndex]?.name}</h2>
             <div style={{justifyContent:'center', alignItems:'center', display:'flex', flexDirection:'column'}}>
               <label>
@@ -103,14 +101,11 @@ function App() {
                 }
                 products.splice(activeProductIndex, 1, temp_obj)
                 setProducts(products)
+                getPercentage(activeProductIndex)
               }}/>
             </div>
-          </div>
-          <div style={{width: '50%', height:'100%', position:'relative'}}>
-            <h2 style={{ zIndex:-1, position:'relative', left:0}}>progress</h2>
-            <h2 style={{ zIndex:1, position:'absolute',  left:0, top:0, backgroundColor:'red', opacity:0.5}}>ks</h2>
-          </div>
       </div>
+      
       }
     </div>
   );
