0a2be8121e34e9c6509e9261c1d1935cdb0212eb
vinayv-456 <vinayvallabineni456@gmail.com>
2021-03-16 22:45:06 +0530

    Add "complete" state variable update on submit
    --for the render on submission

--- src/App.js
+++ src/App.js
@@ -3,105 +3,106 @@ import './App.css';
 
 function App() {
   const [activeProductIndex, setActiveProductIndex] = useState(0);
-  const [products , setProducts] = useState([])
+  const [products, setProducts] = useState([])
 
   const [formTarget, setFormTarget] = useState('');
   const [formAchieved, setFormAchieved] = useState('');
-  
-  useEffect(()=>{
+  const [completed, setCompleted] = useState(0);
+
+  useEffect(() => {
     const temp_products = [
       {
         name: 'abc',
         target: '',
-        achieved: ''  
+        achieved: ''
       },
       {
         name: 'def',
         target: '',
-        achieved: ''  
+        achieved: ''
       },
       {
         name: 'ghi',
         target: '',
-        achieved: ''  
+        achieved: ''
       },
       {
         name: 'jkl',
         target: '',
-        achieved: ''  
+        achieved: ''
       },
       {
         name: 'mno',
         target: '',
-        achieved: ''  
+        achieved: ''
       },
       {
         name: 'pqr',
         target: '',
-        achieved: ''  
+        achieved: ''
       },
       {
         name: 'stu',
         target: '',
-        achieved: ''  
+        achieved: ''
       },
       {
         name: 'vwx',
         target: '',
-        achieved: ''  
+        achieved: ''
       },
       {
         name: 'yza',
         target: '',
-        achieved: ''  
+        achieved: ''
       }
     ]
     setProducts(temp_products);
   }, [])
-  
-  
+
   return (
-    <div className="main" style={{height: '100vh', display:'flex', width:'100vw'}}>
-      <div className="App" style={{position:'relative', flexDirection:'column', overflowY:'scroll', width:'80%'}}>
+    <div className="main" style={{ height: '100vh', display: 'flex', width: '100vw' }}>
+      <div className="App" style={{ position: 'relative', flexDirection: 'column', overflowY: 'scroll', width: '80%' }}>
         {
           products?.map((product, key) => {
-            console.log("products", products);
-            let percentage = product.achieved ? (product.achieved/product.target)*100 : 0;
-            return <div key={key} className={`card testGradiants${key%3} flexVertical`}  onClick={()=> {setActiveProductIndex(key); setFormTarget(product?.target); setFormAchieved(product?.achieved);}}>
-                    <h2 className={'center'}>{product.name}</h2>
-                    <div style={{ height: `${percentage}%`}} className={`transition`}></div>
-                    <p>{product.achieved ? product.achieved : '--'}/{product.target ? product.target : '--'}</p>
-                  </div>
+            let percentage = product.achieved ? (product.achieved / product.target) * 100 : 0;
+            return <div key={key} className={`card testGradiants${key % 3} flexVertical`} onClick={() => { setActiveProductIndex(key); setFormTarget(product?.target); setFormAchieved(product?.achieved); }}>
+              <h2 className={'center'}>{product.name}</h2>
+              <div style={{ height: `${percentage}%` }} className={`transition`}></div>
+              <p>{product.achieved ? product.achieved : '--'}/{product.target ? product.target : '--'}</p>
+            </div>
           })
         }
       </div>
 
-      {products&&
-      <div style={{display:'flex', justifyContent:'center', alignItems:'center', flexDirection:"column"}}>
-            <h2 style={{textAlign:'center', marginTop:'30px', marginBottom:'50px'}}>Enter details of {products[activeProductIndex]?.name}</h2>
-            <div style={{justifyContent:'center', alignItems:'center', display:'flex', flexDirection:'column'}}>
-              <label>
-                Target:
-                <input type="text" name="target" value={formTarget} onChange={(event)=> setFormTarget(event.target.value)}/>
-              </label>
-              <br/>
-              <label>
-                Achieved:
-                <input type="text" name="achieved" value={formAchieved} onChange={(event)=> setFormAchieved(event.target.value)}/>
-              </label>
-              <br/>
-              <input type="submit" value="submit" onClick={()=> {
-                const temp_obj = {
-                  name : products[activeProductIndex].name,
-                  target : formTarget,
-                  achieved: formAchieved
-                }
-                products.splice(activeProductIndex, 1, temp_obj)
-                setProducts(products)
-              }}/>
-            </div>
-      </div>
-      
+      {products &&
+        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: "column" }}>
+          <h2 style={{ textAlign: 'center', marginTop: '30px', marginBottom: '50px' }}>Enter details of {products[activeProductIndex]?.name}</h2>
+          <div style={{ justifyContent: 'center', alignItems: 'center', display: 'flex', flexDirection: 'column' }}>
+            <label>
+              Target:
+                <input type="text" name="target" value={formTarget} onChange={(event) => setFormTarget(event.target.value)} />
+            </label>
+            <br />
+            <label>
+              Achieved:
+                <input type="text" name="achieved" value={formAchieved} onChange={(event) => setFormAchieved(event.target.value)} />
+            </label>
+            <br />
+            {console.log(completed)}
+            <input type="submit" value="submit" onClick={() => {
+              const temp_obj = {
+                name: products[activeProductIndex].name,
+                target: formTarget,
+                achieved: formAchieved
+              }
+              products.splice(activeProductIndex, 1, temp_obj)
+              setProducts(products)
+              setCompleted(!completed);
+            }} />
+          </div>
+        </div>
+
       }
     </div>
   );
